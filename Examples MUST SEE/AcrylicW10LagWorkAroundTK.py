03efad60d0dffb180085db5181a9dbec39ffd7f0
Федор Глеб | Abhay Gaur <59841219+snehccurry@users.noreply.github.com>
2022-07-19 12:44:51 +0530

    Update AcrylicW10LagWorkAroundTK.py

--- Examples MUST SEE/AcrylicW10LagWorkAroundTK.py
+++ Examples MUST SEE/AcrylicW10LagWorkAroundTK.py
@@ -2,17 +2,26 @@
 from tkinter import *
 import ctypes
 from BlurWindow.blurWindow import blur,Win7Blur,GlobalBlur
-
+from ctypes import windll                                               #new import> windll
 root = Tk()
-root.config(bg='green')
+root.config(bg='#202020')                                               #green won't make it blur, I tried a different color, and it seems to work fine.
 
-root.wm_attributes("-transparent", 'green')
+#root.wm_attributes("-transparent", 'green')                            # This is causing the bugs with this kind of weak approach but then it works
 root.geometry('500x400')
 root.update()
 
-HWND = ctypes.windll.user32.GetForegroundWindow()
+mainWindow=root                                                         #defining the main window
+HWND =    windll.user32.GetParent(mainWindow.winfo_id())                #ctypes.windll.user32.GetForegroundWindow() # this line causes the issue, I've replaced it
+dark_mode='#11111199'
+light_mode='#30121244'
+super_dark_mode='#111111FF'
 
-GlobalBlur(HWND,Acrylic=True) #Enable Acrylic
+
+
+
+theme=light_mode
+
+GlobalBlur(HWND,hexColor=theme,Acrylic=True) #Enable Acrylic
 
 global ACRYLIC_ENABLED
 ACRYLIC_ENABLED = True
@@ -24,7 +33,7 @@ def dragging(event):
     global DRAG
     if event.widget is root: #if is event Configure of root (Drag,Resize)
         if DRAG == False:#If Drag is disabled (set by stop_drag)
-            GlobalBlur(HWND,Acrylic=False)
+            GlobalBlur(HWND,hexColor=theme,Acrylic=False)
         else:
             root.after_cancel(DRAG) #cancel task \/ (is dragging)
         DRAG = root.after(200, stop_drag) #execute stop_drag after 200ms
@@ -32,7 +41,7 @@ def dragging(event):
 def stop_drag():
     global DRAG
     DRAG = False
-    GlobalBlur(HWND,Acrylic=True) 
+    GlobalBlur(HWND,hexColor=theme,Acrylic=True) 
 
 root.bind('<Configure>', dragging)
 
